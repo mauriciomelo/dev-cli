84af7fa92c78af84d7a65e18ecb6adeba7058b96
Mauricio Melo <mauricioltw@gmail.com>
2020-12-24 12:12:24 -0300

    fix(000): Avoid alias duplicates :bug:

--- src/scripts.ts
+++ src/scripts.ts
@@ -6,14 +6,17 @@ const path = require('path');
 import './cli-commit';
 import './cli-clone';
 import './cli-update';
+import { Command } from 'commander';
 const { program, parse } = require('./program');
 
 let packageJSON = JSON.parse(fs.readFileSync(path.resolve('./package.json')));
 
+const aliasList = program.commands.map((c: Command) => c._alias);
+
 parsePackage(packageJSON).forEach(cmd => {
   program
     .command(cmd.name)
-    .alias(cmd.alias)
+    .alias(aliasList.includes(cmd.alias) ? undefined : cmd.alias)
     .action(cmd.action);
 });
 
